1543cdc47175312d5c0aec52a886aedfc6beaa1a
Nilson <mgtalsc@gmail.com>
2020-11-22 19:07:26 -0400

    conec front

--- src/main/java/bo/ucb/edu/ingsoft/api/CompanyApi.java
+++ src/main/java/bo/ucb/edu/ingsoft/api/CompanyApi.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 
+@CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
 @RequestMapping(value = "/v1/company")
 public class CompanyApi {
