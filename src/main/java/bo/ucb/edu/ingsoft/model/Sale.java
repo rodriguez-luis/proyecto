4e8f7a83ffad00e3fe50f9124cbca0729adf219d
alvaromoller <47613105+alvaromoller@users.noreply.github.com>
2021-01-09 22:33:46 -0400

    agregando SaleBl

--- src/main/java/bo/ucb/edu/ingsoft/model/Sale.java
+++ src/main/java/bo/ucb/edu/ingsoft/model/Sale.java
@@ -102,6 +102,37 @@ public class Sale {
     public Sale() {
         this.transaction= new Transaction();
     }
+    public Integer getTxId() {
+        return transaction.getTxId();
+    }
+
+    public void setTxId(Integer txId) {
+        this.transaction.setTxId(txId);
+    }
+
+    public String getTxHost() {
+        return transaction.getTxHost();
+    }
+
+    public void setTxHost(String txHost) {
+        this.transaction.setTxHost(txHost);
+    }
+
+    public Integer getTxUserId() {
+        return transaction.getTxUserId();
+    }
+
+    public void setTxUserId(Integer txUserId) {
+        this.transaction.setTxUserId(txUserId);
+    }
+
+    public Date getTxDate() {
+        return transaction.getTxDate();
+    }
+
+    public void setTxDate(Date txDate) {
+        this.transaction.setTxDate(txDate);
+    }
 
     @Override
     public String toString() {
