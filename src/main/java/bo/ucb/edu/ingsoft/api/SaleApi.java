6431491d50e738f3949ef5449ca1e4fa97d189ab
alvaromoller <47613105+alvaromoller@users.noreply.github.com>
2021-01-09 22:42:00 -0400

    agregando metodo create

--- src/main/java/bo/ucb/edu/ingsoft/api/SaleApi.java
+++ src/main/java/bo/ucb/edu/ingsoft/api/SaleApi.java
@@ -2,12 +2,17 @@ package bo.ucb.edu.ingsoft.api;
 
 import bo.ucb.edu.ingsoft.bl.SaleBl;
 import bo.ucb.edu.ingsoft.bl.TransactionBl;
+import bo.ucb.edu.ingsoft.dto.CompanyDto;
+import bo.ucb.edu.ingsoft.dto.SaleDto;
+import bo.ucb.edu.ingsoft.model.Transaction;
+import bo.ucb.edu.ingsoft.util.TransactionUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
 
 @CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
@@ -25,7 +30,15 @@ public class SaleApi {
         this.saleBl = saleBl;
     }
 
-
+    @RequestMapping(method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE,
+            consumes = MediaType.APPLICATION_JSON_VALUE)
+    public SaleDto createSale(@RequestBody SaleDto saleDto, HttpServletRequest request) {
+        // Creamos transaccion para la operación.
+        Transaction transaction = TransactionUtil.createTransaction(request);
+        transactionBl.createTransaction(transaction);
+        SaleDto saleResponse = saleBl.createSale(saleDto, transaction);
+        return saleResponse;
+    }
 
 
 }
